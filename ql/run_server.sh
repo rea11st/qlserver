af4befcc92bc9b622d4901dee6068bfdb532f571
cyc1e <petroprom76@gmail.com>
2025-10-14 23:24:55 +0300

    fix run sh

--- ql/run_server.sh
+++ ql/run_server.sh
@@ -5,7 +5,5 @@ echo "[INFO] Current working directory: $(pwd)"
 echo "[INFO] Listing files:"
 ls -la
 
-echo "[INFO] Trying to run run_server_x64.sh..."
-exec ./run_server_x64.sh
-
-./run_server_x64.sh +set fs_game ql +set zmq_rcon_enable 1 +set zmq_rcon_password strong_rcon_pass +exec server.cfg
+echo "[INFO] Trying to run run_server_x64.sh with parameters..."
+exec ./run_server_x64.sh +set fs_game ql +set zmq_rcon_enable 1 +set zmq_rcon_password "${QL_RCON_PASSWORD}" +exec server.cfg
