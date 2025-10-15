e41b9b3b48e6eedbd600c84a643e709495d5e002
cyc1e <petroprom76@gmail.com>
2025-10-15 14:23:52 +0300

    added steam token

--- ql/run_server.sh
+++ ql/run_server.sh
@@ -6,4 +6,4 @@ echo "[INFO] Listing files:"
 ls -la
 
 echo "[INFO] Trying to run run_server_x64.sh with parameters..."
-exec ./run_server_x64.sh +set fs_game ql +set zmq_rcon_enable 1 +set zmq_rcon_password "${QL_RCON_PASSWORD}" +exec server.cfg
+exec ./run_server_x64.sh +set fs_game ql +set zmq_rcon_enable 1 +set zmq_rcon_password "${QL_RCON_PASSWORD}" +set steam_token "${STEAM_TOKEN}" +exec server.cfg
