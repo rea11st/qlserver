6cc3f65d040382475f483fcca375901b8124edc9
cyc1e <petroprom76@gmail.com>
2025-10-16 13:39:13 +0300

    fix run sh

--- ql/run_server.sh
+++ ql/run_server.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "[INFO] Current working directory: $(pwd)"
-echo "[INFO] Listing files:"
-ls -la
+echo "[INFO] Setting PYTHONPATH for minqlx"
+export PYTHONPATH=/ql/minqlx:$PYTHONPATH
 
 echo "[INFO] Trying to run run_server_x64.sh with parameters..."
 exec ./run_server_x64.sh +set fs_game minqlx +set zmq_rcon_enable 1 +set zmq_rcon_password "${QL_RCON_PASSWORD}" +set steam_token "${STEAM_TOKEN}" +exec server.cfg
